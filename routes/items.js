8ae828aada7dd1cd241a81800447c9632ef15996
Ruanqin <teldrassildruid@gmail.com>
2021-11-10 01:17:59 -0500

    finish item route

--- routes/items.js
+++ routes/items.js
@@ -2,27 +2,36 @@ const express = require("express");
 const router = express.Router();
 const Item = require("../modles/Item");
 
-// @route GET api/items
-// @desc Get all Items
+// @route  GET api/items
+// @desc   Get all Items
 // @access Public
 router.get("/", async (req, res) => {
   const items = await Item.find({});
   res.send(items);
 });
 
-// @route POST api/items
-// @desc Create A Post
+// @route  POST api/items
+// @desc   Create A Post
 // @access Public
 router.post("/", async (req, res) => {
   const newItem = new Item({
     name: req.body.name,
   });
   await newItem.save();
-  res.status(200).json({
+  return res.status(200).json({
     status: 200,
     data: newItem,
     message: "New item saved",
   });
+});
+
+// @route  DELETE api/items/item_id
+// @desc   Delete an item by its _id
+// @access Public
+router.delete("/:item_id", async (req, res) => {
+  const { item_id } = req.params;
+  await Item.findByIdAndDelete(item_id);
+  res.send("Item deleted!");
 });
 
 module.exports = router;
