7cb90e551f1391449d4c56c29e6a290e87b24d51
Ruanqin <teldrassildruid@gmail.com>
2021-11-12 01:17:53 -0500

    add info to product detail screen

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import Rating from './Rating';
 
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded' bg='light'>
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
-      </a>
+      </Link>
       <Card.Body>
-        <a href={`/product/${product._id}`} className='link-primary'>
+        <Link to={`/product/${product._id}`} className='link-primary'>
           <Card.Title>{product.name}</Card.Title>
-        </a>
+        </Link>
         <Card.Text as='div'>
           <Rating
             value={product.rating}
--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,9 +1,80 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  ListGroupItem,
+} from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
 
 const ProductScreen = () => {
   const { id } = useParams();
-  return <div>Product id: {id}</div>;
+
+  const product = products.find((p) => p._id === id);
+  return (
+    <div>
+      <Link className='btn btn-light my-3' to='/'>
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroupItem>
+              <h3>{product.name}</h3>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroupItem>
+            <ListGroupItem>Price: ${product.price}</ListGroupItem>
+            <ListGroupItem>Description: ${product.description}</ListGroupItem>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroupItem>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong> ${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroupItem>
+
+              <ListGroupItem>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                  </Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem className='d-grid gap-2'>
+                <Button
+                  className='btn btn-primary'
+                  type='button'
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
+                </Button>
+              </ListGroupItem>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default ProductScreen;
