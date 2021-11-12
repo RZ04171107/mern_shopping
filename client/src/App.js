ac7ad9189f272fe7242bcc961e19bc0e5f653cf1
Ruanqin <teldrassildruid@gmail.com>
2021-11-12 00:21:05 -0500

    create product detail screen, add 2 routes: homepage & prodect detail

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
@@ -10,7 +12,10 @@ function App() {
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path='/' exact element={<HomeScreen />} />
+            <Route path='/product/:id' exact element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
