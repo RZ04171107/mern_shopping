58bb6a6e392a8ae5e742de2b22c860130fd43e2e
Ruanqin <teldrassildruid@gmail.com>
2021-11-13 00:10:08 -0500

    update models

--- server/modles/Product.js
+++ server/modles/Product.js
@@ -1,18 +1,65 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ProductSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const ReviewSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
   },
-  image: String, // String is shorthand for  {type: String}
-  description: String,
-  brand: String,
-  category: String,
-  price: Number,
-  countInStock: Number,
-  rating: Number,
-  numReviews: Number,
-});
+  {
+    timestamps: true,
+  }
+);
+
+const ProductSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: String, // String is shorthand for  {type: String}
+    description: String,
+    brand: String,
+    category: String,
+    price: {
+      type: Number,
+      required: true,
+      defaule: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      defaule: 0,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    reviews: [ReviewSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
 module.exports = mongoose.model('Product', ProductSchema);
