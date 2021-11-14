dd5690e1f88684e249563e8639738825d106f6de
Ruanqin <teldrassildruid@gmail.com>
2021-11-14 03:53:09 -0500

    updata product route

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,21 +1,34 @@
 const express = require('express');
+const asyncHandler = require('express-async-handler');
 const router = express.Router();
 const Product = require('../modles/Product');
 
 // @route  GET api/products
-// @desc   Get all Products
+// @desc   Fetch all Products
 // @access Public
-router.get('/', async (req, res) => {
-  const products = await Product.find({});
-  res.send(products);
-});
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.send(products);
+  })
+);
 
 // @route  GET api/products/:id
-// @desc   Get a product
+// @desc   Fetch single product
 // @access Public
-router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  //const product = products.find((p) => p._id === id);
-  const product = await Product.findById(id);
-  res.send(product);
-});
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).json({ message: 'Product Not Found' });
+    }
+  })
+);
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,8 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8000;
 const connectDB = require('./config/db');
-const products = require('./data/products');
-//const productRoutes = require('./routes/product');
+const productRoutes = require('./routes/product');
 const dotenv = require('dotenv');
 dotenv.config();
 connectDB();
@@ -15,21 +14,21 @@ app.use(bodyParser.json());
 
 // use Routes
 // app.use('/api/items', items);
-// app.use('/api/products', productRoutes);
+app.use('/api/products', productRoutes);
 
 app.get('/test', (req, res) => {
   res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-  res.send(products);
-});
+// app.get('/api/products', (req, res) => {
+//   res.send(products);
+// });
 
-app.get('/api/products/:id', (req, res) => {
-  const { id } = req.params;
-  const product = products.find((p) => p._id === id);
-  res.send(product);
-});
+// app.get('/api/products/:id', (req, res) => {
+//   const { id } = req.params;
+//   const product = products.find((p) => p._id === id);
+//   res.send(product);
+// });
 
 app.listen(port, () =>
   console.log(`Server running in ${process.env.NODE_ENV} on port ${port}`)
